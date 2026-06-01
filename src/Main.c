a4402f75eab188f45b7f8003c856484d9adfdf58
codeleaded <gnu.c.reaper@gmail.com>
2026-06-01 02:48:21 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -8,11 +8,11 @@ int main(){
     FilesSize size;
     char* data = Files_ReadB(Path,&size);
     
-    printf("Beginning: S: %d\n",size);
+    printf("Beginning: S: %u\n",size);
     void* compressed = Zip_Byte_zip(data,(long long[]){ size });
-    printf("Compressed: S: %d\n",size);
+    printf("Compressed: S: %u\n",size);
     void* opened = Zip_Byte_open(compressed,(long long[]){ size });
-    printf("Opened: S: %d\n",size);
+    printf("Opened: S: %u\n",size);
 
     Files_Write(Path,opened,size);
 
